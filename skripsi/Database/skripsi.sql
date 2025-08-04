d37470a51dc2aa574faecfbbe0147146291dfc6e
yohaneshenz17 <yohaneshenz@gmail.com>
2025-08-05 08:15:04 +0900

    Update skripsi.sql

--- skripsi/Database/skripsi.sql
+++ skripsi/Database/skripsi.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Aug 04, 2025 at 07:17 PM
+-- Generation Time: Aug 05, 2025 at 06:05 AM
 -- Server version: 10.3.39-MariaDB-cll-lve
 -- PHP Version: 8.1.33
 
